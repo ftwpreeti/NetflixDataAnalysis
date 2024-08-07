503a9defde8fcc8f096c80cb92db53fe15c2fe5b
Preeti Priyadarshini <preeti.rourkela@gmail.com>
2024-08-07 11:27:15 +0530

    Add files via upload

---
+++ NetflixDataTable.sql
@@ -0,0 +1,20 @@
+# Netflix Dataset Table
+
+CREATE TABLE NetflixTable 
+(
+    id	VARCHAR(512),
+    title	VARCHAR(512),
+    type	VARCHAR(512),
+    description	VARCHAR(512),
+    release_year	INT,
+    age_certification	VARCHAR(512),
+    runtime	INT,
+    genres	VARCHAR(512),
+    production_countries	VARCHAR(512),
+    seasons	VARCHAR(512),
+    imdb_id	VARCHAR(512),
+    imdb_score	DOUBLE,
+    imdb_votes	INT,
+    tmdb_popularity	DOUBLE,
+    tmdb_score	DOUBLE
+);
